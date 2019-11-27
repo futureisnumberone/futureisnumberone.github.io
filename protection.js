17ea66ba6b9daefe1db6229a26a29bfac01fc948
futureisnumberone <58135027+futureisnumberone@users.noreply.github.com>
2019-11-26 21:42:50 -0500

    Add files via upload

---
+++ protection.js
@@ -0,0 +1,10 @@
+var password = "password1234";
+var input = prompt("Enter the password:");
+
+if(input == password) {
+    window.open('meandmonte.html');
+}
+
+else {
+    window.open('wrongpass.html')
+}
